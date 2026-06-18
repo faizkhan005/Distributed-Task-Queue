bd41716ae9e77ab920148f3929d6c7e6f0670577
Faizan Ahmed Khan R K <faizanahmedkhan005@gmail.com>
2026-06-18 19:23:34 -0400

    Added DB Context and Necessariy nuget packages

---
+++ TaskQueue/TaskQueue.Infrastructure/Persistence/TaskQueueDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskQueue.Domain.Entities;
+
+namespace TaskQueue.Infrastructure.Persistence;
+
+public class TaskQueueDbContext : DbContext
+{
+    public TaskQueueDbContext(DbContextOptions<TaskQueueDbContext> options) : base(options) { }
+
+    public DbSet<JobRecord> JobRecords => Set<JobRecord>();
+    public DbSet<JobFailure> JobFailures => Set<JobFailure>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(TaskQueueDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+}
