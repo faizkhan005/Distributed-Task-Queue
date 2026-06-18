10f06cc82c859847656011e015153ac8b42192af
Faizan Ahmed Khan R K <faizanahmedkhan005@gmail.com>
2026-06-18 19:11:41 -0400

    Added Job Interfaces

---
+++ TaskQueue/TaskQueue.Domain/Interfaces/IJobRecordRepository.cs
@@ -0,0 +1,13 @@
+﻿using TaskQueue.Domain.Entities;
+using TaskQueue.Domain.Enums;
+
+namespace TaskQueue.Domain.Interfaces;
+
+public interface IJobRecordRepository
+{
+    Task<JobRecord?> GetByIdAsync(Guid id, CancellationToken ct = default);
+    Task<JobRecord?> GetByHangfireIdAsync(string hangfireJobId, CancellationToken ct = default);
+    Task<IReadOnlyList<JobRecord>> GetByStatusAsync(JobStatus status, CancellationToken ct = default);
+    Task AddAsync(JobRecord record, CancellationToken ct = default);
+    Task UpdateAsync(JobRecord record, CancellationToken ct = default);
+}
