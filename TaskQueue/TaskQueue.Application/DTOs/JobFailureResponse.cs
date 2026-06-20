a2f99bb33fdba3cd16ea2ac2c1435224ac92dc62
Faizan Ahmed Khan R K <faizanahmedkhan005@gmail.com>
2026-06-20 14:16:07 -0400

    Cretaed Additional DTOs

---
+++ TaskQueue/TaskQueue.Application/DTOs/JobFailureResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskQueue.Application.DTOs;
+
+public record JobFailureResponse(
+    Guid Id,
+    Guid JobRecordId,
+    string JobType,
+    string ErrorMessage,
+    int TotalAttempts,
+    DateTime FailedAt,
+    bool Requeued,
+    DateTime? RequeuedAt
+);
