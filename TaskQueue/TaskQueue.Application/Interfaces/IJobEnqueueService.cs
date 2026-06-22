83bd61bef9d129f5089a443b6d8c878e89304b13
Faizan Ahmed Khan R K <faizanahmedkhan005@gmail.com>
2026-06-22 10:57:34 -0400

    Created IJobEnqueService Interface

---
+++ TaskQueue/TaskQueue.Application/Interfaces/IJobEnqueueService.cs
@@ -0,0 +1,18 @@
+﻿using TaskQueue.Application.DTOs;
+
+namespace TaskQueue.Application.Interfaces;
+
+public interface IJobEnqueueService
+{
+    Task<EnqueueJobResponse> EnqueueNotificationAsync(
+       EnqueueNotificationRequest request,
+       CancellationToken ct = default);
+
+    Task<EnqueueJobResponse> EnqueueReportGenerationAsync(
+        EnqueueReportRequest request,
+        CancellationToken ct = default);
+
+    Task<EnqueueJobResponse> EnqueueDataSyncAsync(
+        EnqueueDataSyncRequest request,
+        CancellationToken ct = default);
+}
